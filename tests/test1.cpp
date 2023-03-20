703a46e6a7a002b87d180cfbbc3142c8d814f226
MikhalevaSofia <MixSo.net@yandex.ru>
2023-03-20 12:48:47 -0400

    upd

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -28,7 +28,7 @@ EXPECT_EQ(acc.Account::GetBalance(), 100);
 TEST(Account, ChangeBalance){
 MockAccount acc(0, 100);
 EXPECT_THROW(acc.Account::ChangeBalance(50), std::runtime_error);
-acc.Account::Lock();
+//acc.Account::Lock();
 acc.Account::ChangeBalance(50);
 EXPECT_EQ(acc.Account::GetBalance(), 50);
 
