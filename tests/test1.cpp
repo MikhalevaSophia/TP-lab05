b14dcb728dbec38a068d9292de218a93ea564740
MikhalevaSophia <112760887+MikhalevaSophia@users.noreply.github.com>
2023-03-22 21:01:10 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -17,6 +17,9 @@ TEST(Account, Init){
 
 class MockTransaction: public Transaction{
 public:
+    MOCK_METHOD(void, set_fee, (int fee), (override));
+    MOCK_METHOD(void Credit, (Account& accout, int sum), (override));
+    MOCK_METHOD(bool Debit, (Account& accout, int sum), (override));
     MOCK_METHOD(void, SaveToDataBase, (Account& from, Account& to, int sum), (override));
 };
 
@@ -54,6 +57,31 @@ EXPECT_THROW(tr.Make(from_acc,from_acc,0),std::logic_error);
 EXPECT_THROW(tr.Make(from_acc,to_acc,0),std::logic_error);
 EXPECT_THROW(tr.Make(from_acc,to_acc,-5),std::invalid_argument);
 }
+
+
+TEST(Transaction, set_fee){
+MockTransaction tr;
+tr.set_fee(5);
+EXPECT_EQ(tr.fee(),5);
+}
+
+TEST(Transaction, Credit) {
+MockAccount acc(15,100);
+MockTransaction tr;
+tr.Credit(acc,200);
+EXPECT_EQ(acc.GetBalance(),200);
+}
+
+TEST(Transaction, Debit) {
+MockAccount acc(15,100);
+MockTransaction tr;
+tr.Debit(acc,200);
+EXPECT_EQ(acc.GetBalance(),300);
+
+}
+
+
+
 
 TEST(Transaction, SaveToDataBase){
 MockAccount from_acc(15,17650);
